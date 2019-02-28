9da189e94433592a37b348625ccdc209f83f1433
omer <Omerler@gmail.com>
2019-02-28 21:10:45 +0200

    fix score

--- score.py
+++ score.py
@@ -2,14 +2,13 @@ from slide import *
 from img import *
 from data import *
 
-
 def score_two(s1, s2):
     s1_tags = s1.get_tags()
     s2_tags = s2.get_tags()
-    inter = s1_tags.intersection(s2_tags)
-    left = s1_tags - s2_tags
-    right = s2_tags - s1_tags
-    return min([inter, left, right])
+    inter = len(s1_tags.intersection(s2_tags))
+    left = len(s1_tags - s2_tags)
+    right = len(s2_tags - s1_tags)
+    return min(inter, left, right)
 
 def score_all(solution):
     total_score = 0
